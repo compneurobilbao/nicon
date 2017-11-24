5208cc4b1085914c28a0ae608320463a6ec02052
erramuzpe <asier.erramuzpe@gmail.com>
2017-11-24 11:43:55 +0100

    modified general structure

--- nicon/nicon.py
+++ nicon/nicon.py
@@ -1,71 +1,55 @@
 # -*- coding: utf-8 -*-
-import os
-from os.path import join as opj
+import sys
 
+from external_calls import (run_mriqc,
+                            run_fmriprep,
+                            extract_timeseries,
+                            calc_fc_matrix,
+                            run_ica_rsn,
+                            correct_dwi,
+                            run_ndmg
+                            )
+
+
+# TODO: All this global variables should go in an external file
 DATA_DIR = '/home/asier/git/nicon/nicon/data'
 subject = 'sub-001'
 OUTPUT_DIR = '/home/asier/Desktop/nicon_test/output'
 WORK_DIR = '/home/asier/Desktop/nicon_test/workdir'
+INPUT_ATLAS_PATH = ''
 
 
-# optional: mriqc
-# NOTE: OUTPUT -> better to put it's own folder. opj(OUTPUT_DIR, 'mriqc')
+def run_nicon():
 
-# perform: fmriprep
-# perform extract series nilearn
+    # load global variables
 
-# perform ICA -> RSN (DMN...)
+    # optional: mriqc
+    if qc:
+        run_mriqc()
+        # NOTE: OUTPUT -> better to put it's own folder.
+        # opj(OUTPUT_DIR, 'mriqc')
 
-# perform (optional?) dwi motion correction
-# perform ndmg
+    # fmriprep
+        # optional: freesurfer
+    # extract series nilearn
+    # calculate FC matrix
+    # optional: ICA -> RSN (DMN...)
+    if fmri:
+        run_fmriprep()
+        extract_timeseries()
+        calc_fc_matrix()
+        if ica_rsn:
+            run_ica_rsn()
 
-import subprocess
+    # optional: dwi motion correction
+    # ndmg
+    if dwi:
+        if dwi_correction:
+            correct_dwi()
+        run_ndmg()
+        
 
-def execute(cmd):
-    popen = subprocess.Popen(cmd,
-                             stdout=subprocess.PIPE,
-                             universal_newlines=True)
-    for stdout_line in iter(popen.stdout.readline, ""):
-        yield stdout_line
-    popen.stdout.close()
-    return_code = popen.wait()
-    if return_code:
-        raise subprocess.CalledProcessError(return_code, cmd)
-
-
-def run_fmriprep():
-
-    command = [
-           'docker', 'run', '-i', '--rm',
-           '-v', DATA_DIR + ':/data:ro',
-           '-v', OUTPUT_DIR + ':/output',
-           '-v', WORK_DIR + ':/work',
-           '-w', '/work',
-           'poldracklab/fmriprep:latest',
-           '/data', '/output', 'participant',
-           '--participant_label', subject,
-           '-w', '/work', '--no-freesurfer', '--ignore', 'fieldmaps',
-           '--output-space', 'template',
-           '--template', 'MNI152NLin2009cAsym',
-        ]
-
-    for output in execute(command):
-        print(output)
-
-
-def run_mriqc():
-
-    command = [
-           'docker', 'run', '-i', '--rm',
-           '-v', DATA_DIR + ':/data:ro',
-           '-v', OUTPUT_DIR + ':/output',
-           '-v', WORK_DIR + ':/work',
-           '-w', '/work',
-           'poldracklab/mriqc:latest',
-           '/data', '/output', 'participant',
-           '--participant_label', subject,
-           '-w', '/work', '--verbose-reports',
-        ]
-
-    for output in execute(command):
-        print(output)
+if __name__ == "__main__":
+    
+    run_nicon()
+    sys.exit()

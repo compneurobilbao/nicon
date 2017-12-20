46363d9ce2cf137e0ee0389d7f44485b7d2db4ba
erramuzpe <asier.erramuzpe@gmail.com>
2017-12-20 12:50:24 +0100

    added config.py file

---
+++ nicon/config.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+DATA_DIR = ''
+SUBJECT = 'sub-001'
+OUTPUT_DIR = '/home/output_nicon'
+WORK_DIR = '/tmp/tmp_nicon'
+
+QC = False
+FMRI = True
+ICA_RSN = False
+DWI = True
+DWI_CORRECTION = False
